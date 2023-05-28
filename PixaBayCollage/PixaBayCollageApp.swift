1a2353e4df281665853ec0f5ae461a8de4c9ee07
Zulqurn <101562957+Zulqurn@users.noreply.github.com>
2023-05-28 16:58:46 +0100

    Initial Commit

---
+++ PixaBayCollage/PixaBayCollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixaBayCollageApp.swift
+//  PixaBayCollage
+//
+//  Created by Muhammad Zulqurnain on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PixaBayCollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
